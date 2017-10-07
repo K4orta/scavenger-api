3a74f76f3c3c3499c090072ba96b66c7f6bd4d52
K4Orta <k4orta@gmail.com>
2017-10-07 13:40:30 -0700

    Add phone query

--- controllers/tries.js
+++ controllers/tries.js
@@ -4,7 +4,7 @@ function get(req, res) {
   const client = new Client()
   client.connect()
 
-  client.query('SELECT * FROM tries').then(resp => {
+  client.query(`SELECT * FROM tries WHERE user_phone = '+${req.query.phone}';`).then(resp => {
     res.json(resp.rows)
     client.end()
   }).catch(err => {
